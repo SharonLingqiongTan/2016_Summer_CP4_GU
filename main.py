c4f8e2f5d12161cea9f176c9b74af9e4d21ccf1b
Jianxian <wjxtank@gmail.com>
2016-11-28 06:00:31 -0500

    haha

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from multiprocessing import Pool
 import yaml
 from fuzzywuzzy import fuzz
+from bs4 import BeautifulSoup
 from pymongo import MongoClient
 import search,extraction,ebola_html_dealer
 from elasticsearch import Elasticsearch
@@ -13,14 +14,18 @@ def main():
     reload(sys)
     sys.setdefaultencoding("utf-8")
     query_path = "spql_self_built_query.json"
+    answer_path = "answer.json"
     query_list = search.query_retrival(query_path)
     aggregate = filter(lambda k:k["type"] == "aggregate",query_list)
     aggregate = filter(lambda k:k["id"] == "1223.11",aggregate)
     #global db
     # pool = Pool()
     # pool.map(pipeline,aggregate)
+    result = []
     for query in aggregate:
-        pipeline(query)
+        result.append(pipeline(query,False))
+    f = open(answer_path,"w")
+    json.dump(result,f)
 
 def createIndex(index,type,es,doc):
     malpath = "mal.json"
@@ -51,18 +56,53 @@ def index(index_name):
         createIndex(index_name,document["content_type"],es,document)
     print((datetime.now()-start).seconds)
 
+def TLD_specific_search(document):
+	TLD = extraction.top_level_domain_pattern(document)
+	raw_content = document["raw_content"]
+	# raw_content = document
+	if TLD and raw_content:
+		soup = BeautifulSoup(raw_content, 'html.parser')
+		if TLD == "escortcafe.com":
+			content = soup.find_all("div", class_="details")
+	# print type(content)
+	# print re.findall('Blonde', content)
+		elif TLD == "classifriedads.com":
+			content = soup.find_all(id="contentcell")
+		elif TLD == "slixa.com":
+			content = soup.find_all("div", class_="span9 profile-content") + soup.find_all("aside", class_="profile-sidebar span3")
+		# elif TLD == "allsexyescort.com":
+		elif TLD == "escort-ads.com":
+			content = soup.findall("div", class_="container main-content vip-content")
+		# elif TLD == "liveescortreviews.com":
+		# elif TLD == "escort-europe.com":
+		elif TLD == "find-escorts.com":
+			content = soup.findall(id="contentcell")
+		elif TLD == "escortserv.com":
+			content = soup.findall(id="index")
+		elif TLD == "slixa.ca":
+			content = soup.find_all("div", class_="span9 profile-content") + soup.find_all("aside", class_="profile-sidebar span3")
+		elif TLD == "escortpost.com":
+			content = soup.findall(id="content")
+		elif TLD == "privateescorts.ro":
+			content = soup.findall("tbody")
+		elif TLD == "adultsearch.com":
+			content = soup.findall(id="ad")
 
-def pipeline(query):
+		return str(content)
+	else:
+			return ""
+
+def pipeline(query,tense):
     #client = MongoClient()
     #db = client.database
     start = datetime.now()
     answer_dic = []
     #if query["type"] == "aggregate" and query["id"] == "1223.12":
         #print(query)
-    filepath = "pointfact_test/"+query["id"]
+    # filepath = "pointfact_test/"+query["id"]
     parsed_query_dic = search.query_parse(query)
     result = []
-    if query["type"] == "cluster":
+    if query["type"] == "Cluster Identification":
         result = cluster(query,5)
     else:
         query_body = search.query_body_build(parsed_query_dic)
@@ -102,7 +142,10 @@ def pipeline(query):
             # documents[i]["indexing"] = extractions
             # json.dump(documents[i],w)
             # w.close()
-            if validate(documents[i],parsed_query_dic,True):
+            TLD =  TLD_specific_search(documents[i])
+            if TLD:
+                documents[i]["TLD"] = TLD
+            if validate(documents[i],parsed_query_dic,tense):
                 answer = answer_extraction(documents[i],parsed_query_dic)
                 if answer:
                     dic = {}
@@ -111,13 +154,17 @@ def pipeline(query):
                     dic["els_score"] = documents[i]["_score"]
                     dic["answer"] = answer
                     result.append(dic)
+                else:
+                    if not tense:
+                        return pipeline(query,True)
     final_result = generate_formal_answer(query,result)
-    final_result["timelog"] = (datetime.now()-start).seconds
+    #final_result["timelog"] = (datetime.now()-start).seconds
     #answer_dic.append(final_result)
     #print(answer_dic)
-    f = open(filepath,"w")
-    json.dump(final_result,f)
-    f.close()
+    # f = open(filepath,"w")
+    # json.dump(final_result,f)
+    # f.close()
+    return final_result
 
 def annotator(documents):
     """
@@ -285,7 +332,7 @@ def generate_formal_answer(query,result):
     candidates = []
     parsed_query_dic = search.query_parse(query)
     #print(result)
-    if query["type"] == "cluster":
+    if query["type"] == "Cluster Identification":
         for item in result:
             document_id = item["id"]
             answer_text = item["answer"][0][0]
@@ -294,7 +341,7 @@ def generate_formal_answer(query,result):
         candidates.sort(key = lambda k:k[2],reverse = True)
         threhold = 0.5
         final_result["answers"] = filter(lambda k:k[2]>threhold,candidates)
-    elif query["type"] == "pointfact":
+    elif query["type"] == "Point Fact":
         for item in result: #Currently pick the most relavant answer in each document
             document_id = item["id"]
             answer_text = item["answer"][0][0] #Currently pick the most relavant answer in each document
@@ -312,27 +359,30 @@ def generate_formal_answer(query,result):
         candidates.sort(key = lambda k:k[2],reverse = True)
         type_value = []
         threhold = 0.5
-        if query["type"] == "MODE":
-            value_counter = collections.Counter(k[0] for k in candidates)
-            if parsed_query_dic["group"]["order_by"] == "DESC":
-                value_list = value_counter.sort(key = lambda k:k[1],reverse = True)
-                type_value = value_list[:parsed_query_dic["limit"]]
-                final_result["answers"] = type_value+filter(lambda k:k[2]>threhold,candidates)
-            else:
-                value_list = value_counter.sort(key = lambda k:k[1],reverse = True)
-                type_value = value_list[:parsed_query_dic["limit"]]
-                final_result["answers"] = type_value+filter(lambda k:k[2]>threhold,candidates)
+        if query["type"] == "Cluster Facet":
+            final_result["answers"] = filter(lambda k:k[2]>threhold,candidates)
         else:
-            candidates.sort(key = lambda k:k[0], reverse = True)
-            threhold = 0.5
-            if type == "MIN":
-                type_value = candidates[0][0]
-            elif type == "MAX":
-                type_value = candidates[-1][0]
-            elif type == "AVG":
-                type_value = sum(k[0] for k in candidates)/len(candidates)
-            candidates.sort(key = lambda k:k[2],reverse = True)
-            final_result["answers"] = [type_value]+ filter(lambda k:k[2]>threhold,candidates)
+            if query["type"] == "MODE":
+                value_counter = collections.Counter(k[0] for k in candidates)
+                if parsed_query_dic["group"]["order_by"] == "DESC":
+                    value_list = value_counter.sort(key = lambda k:k[1],reverse = True)
+                    type_value = value_list[:parsed_query_dic["limit"]]
+                    final_result["answers"] = type_value+filter(lambda k:k[2]>threhold,candidates)
+                else:
+                    value_list = value_counter.sort(key = lambda k:k[1],reverse = True)
+                    type_value = value_list[:parsed_query_dic["limit"]]
+                    final_result["answers"] = type_value+filter(lambda k:k[2]>threhold,candidates)
+            else:
+                candidates.sort(key = lambda k:k[0], reverse = True)
+                threhold = 0.5
+                if type == "MIN":
+                    type_value = candidates[0][0]
+                elif type == "MAX":
+                    type_value = candidates[-1][0]
+                elif type == "AVG":
+                    type_value = sum(k[0] for k in candidates)/len(candidates)
+                candidates.sort(key = lambda k:k[2],reverse = True)
+                final_result["answers"] = [type_value]+ filter(lambda k:k[2]>threhold,candidates)
     return final_result
 
 
@@ -370,6 +420,8 @@ def answer_extraction(document,parsed_query_dic):
     extraction_result = []
     answer_field = parsed_query_dic["answer_field"].keys()
     feature = answer_field[0] #only one attibutue will be questioned
+    if feature == "tattoos":
+        return []
     raw_result = extraction.functionDic[feature](document,True,False) #answer from raw_content
     result = extraction.functionDic[feature](document,False,False) #answer from extracted_text
     if result and raw_result:
