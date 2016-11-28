0ab3f3080e2e8659dcd575b2d2edb913dc553b3d
SharonLingqingTan <tanlingqiong@gmail.com>
2016-11-28 07:03:04 -0500

    pip install shell script

---
+++ pipInstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pip install fuzzywuzzy
+pip install elasticsearch
+pip install certifi
+pip install pyyaml
+pip install webcolors
+pip install nltk
+pip install pymongo
+pip install bs4
+
